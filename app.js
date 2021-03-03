1e26b372a263cb7b58241f559298a498973ea18e
centerRyo <abc.atrn@gmail.com>
2021-03-03 21:17:24 +0900

    テトリスを回転させる

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      // rotate();
+      rotate();
     } else if (e.keyCode === 39) {
       moveRight();
     } else if (e.keyCode === 40) {
@@ -116,22 +116,30 @@ document.addEventListener('DOMContentLoaded', () => {
     draw();
   }
 
-    // move the tetromino right, unless is at the edge or there is a block
-    function moveRight() {
-      undraw();
-      const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
+  // move the tetromino right, unless is at the edge or there is a block
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
 
-      if (!isAtRightEdge) currentPosition += 1;
+    if (!isAtRightEdge) currentPosition += 1;
 
-      if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-        currentPosition -= 1;
-      }
-
-      draw();
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
     }
 
+    draw();
+  }
 
-
+  // rotate the tetromino
+  function rotate() {
+    undraw();
+    currentRotation ++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
+    draw();
+  }
 
 
 
